409d75524c9c90266244d82563f7cc700941a93c
Khulood Batis <khuloodbatis@gmail.com>
2022-05-24 11:44:04 +0300

    category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,8 +22,14 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function __invoke()
     {
-        return view('home');
+        //$categories = Category::get();
+        $categories = Category::tree()->get()->toTree();
+
+
+        return view('home',[
+            'categories' => $categories
+        ]);
     }
 }
---
+++ resources/views/components/category.blade.php
@@ -0,0 +1,10 @@
+<div  class="py-2">
+
+   <a href="/categories/{{$category->slug}}" class ="{{ $category->depth === 0 ? 'font-bold':''}}" > {{ $category->title }}</a>
+
+    @foreach ($category->children as $child )
+
+    <x-category :category="$child"/>
+
+    @endforeach
+</div>
